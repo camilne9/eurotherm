6d2d60138258015c183654766acdf751ce220c94
= <=>
2020-11-05 14:29:33 -0500

    spelling error on setpoint

--- test_script.py
+++ test_script.py
@@ -16,23 +16,23 @@ print('\n Target Setpoint: \n')
 print(myEuro.setpoint)
 
 measured_temperatures.append(myEuro.temperature)
-setpoint_temperatures.append(myEuro.setPoint)
+setpoint_temperatures.append(myEuro.setpoint)
 
 # I set the temperature goal, wait for it to reach it
 #for temp in [25, 40, 25]:
-myEuro.setPoint(25)
+#myEuro.setPoint(25)
 
 print(myEuro.workingSetpoint)
-print(myEuro.setPoint)
+print(myEuro.setpoint)
 
 for i in range(30):
     print('\n Working Setpoint: \n')
     print(myEuro.workingSetpoint)
     print('\n Target Setpoint: \n')
-    print(myEuro.setPoint)
+    print(myEuro.setpoint)
     time.sleep(10)
     measured_temperatures.append(myEuro.temperature)
     print(measured_temperatures[-1])
-    setpoint_temperatures.append(myEuro.setPoint)
+    setpoint_temperatures.append(myEuro.setpoint)
     print(setpoint_temperatures[-1])
     i += 1
