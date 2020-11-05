516daea67b0fbb6989b9756446288fd4e27c9bd1
= <=>
2020-11-05 14:23:02 -0500

    isolated one step

--- test_script.py
+++ test_script.py
@@ -19,18 +19,19 @@ measured_temperatures.append(myEuro.temperature)
 setpoint_temperatures.append(myEuro.setPoint)
 
 # I set the temperature goal, wait for it to reach it
-for temp in [25, 40, 25]:
-    myEuro.setPoint(temp)
+#for temp in [25, 40, 25]:
+myEuro.setPoint(temp)
 
+print(myEuro.workingSetpoint)
+
+for i in range(30):
+    print('\n Working Setpoint: \n')
     print(myEuro.workingSetpoint)
-
-    for i in range(10):
-        print('\n Working Setpoint: \n')
-        print(myEuro.workingSetpoint)
-        print('\n Target Setpoint: \n')
-        print(myEuro.setPoint)
-        time.sleep(30)
-        measured_temperatures.append(myEuro.temperature)
-        print(measured_temperatures[-1])
-        setpoint_temperatures.append(myEuro.setPoint)
-        i += 1
+    print('\n Target Setpoint: \n')
+    print(myEuro.setPoint)
+    time.sleep(10)
+    measured_temperatures.append(myEuro.temperature)
+    print(measured_temperatures[-1])
+    setpoint_temperatures.append(myEuro.setPoint)
+    print(setpoint_temperatures[-1])
+    i += 1
