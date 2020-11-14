018f3dd053452ccf38332137c9bfe5e261116164
= <=>
2020-11-14 17:53:15 -0500

    changed formula to have 8000 as a hex

--- test_source.py
+++ test_source.py
@@ -3,18 +3,18 @@ import minimalmodbus
 instrument = minimalmodbus.Instrument("/dev/ttyUSB1", 1)
 
 def convert_to_hex_register(register):
-    return int(str((2*register+8000)),16)
+    return int(str((2*register+32768)),16)
 
-register = 2
-decimals = 1
+register = 24
+decimals = 0
 read_register_output = instrument.read_register(register, decimals)
 print(read_register_output)
 read_float_output = instrument.read_float(convert_to_hex_register(register))
 print(read_float_output)
 
 writing_value = 15
-# write_register_output = instrument.write_register(register, writing_value, decimals)
-# print(write_register_output)
+write_register_output = instrument.write_register(register, writing_value, decimals)
+print(write_register_output)
 write_float_output = instrument.write_float(convert_to_hex_register(register), writing_value)
 print(write_float_output)
 
