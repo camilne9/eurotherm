b37c4d8ab4185353d2d6a55f616ed90b667c0395
= <=>
2020-11-14 17:28:55 -0500

    trying write float on target setpoint

--- test_source.py
+++ test_source.py
@@ -5,7 +5,7 @@ instrument = minimalmodbus.Instrument("/dev/ttyUSB1", 1)
 def convert_to_hex_register(register):
     return int(str((2*register+8000)),16)
 
-register = 3
+register = 2
 decimals = 1
 read_register_output = instrument.read_register(register, decimals)
 print(read_register_output)
@@ -13,8 +13,8 @@ read_float_output = instrument.read_float(convert_to_hex_register(register))
 print(read_float_output)
 
 writing_value = 15
-write_register_output = instrument.write_register(register, writing_value, decimals)
-print(write_register_output)
+# write_register_output = instrument.write_register(register, writing_value, decimals)
+# print(write_register_output)
 write_float_output = instrument.write_float(convert_to_hex_register(register), writing_value)
 print(write_float_output)
 
